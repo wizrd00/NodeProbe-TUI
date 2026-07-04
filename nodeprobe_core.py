462741a10db78dbe81083c6eab15bc9e2b0cbc6b
wizrd <wizrd.bin@proton.me>
2026-07-04 20:52:38 +0330

    start implementing shared library handler

--- nodeprobe_core.py
+++ nodeprobe_core.py
@@ -1,16 +1,48 @@
+from ctypes import *
 from nodeprobe_types import *
+import os
+import socket
+import re
 
+class Arpman :
+	def __init__(self, iface : str, timeout : int, src_mac : str, out_mac : str, src_ip : str, dst_ip : str) :
+		self.context = arpman_context_t(
+			ifindex = self.__get_ifindex(iface),
+			timeout = self.__get_timeout(timeout),
+			src_mac = self.__get_mac(src_mac),
+			out_mac = self.__get_mac(out_mac),
+			src_ip = self.__get_ip(src_ip),
+			dst_ip = self.__get_ip(dst_ip)
+		)
+
+	def __get_ifindex(self, iface : str) -> c_int :
+		return c_int(socket.if_nametoindex(iface))
+
+	def __get_timeout(self, timeout : int) -> c_int :
+		return c_int(timeout)
+
+	def __get_mac(self, mac : str) -> Mac :
+		if (not re.match(r"^((?:[a-fA-F0-9]{2}[:]){5}[a-fA-F0-9]{2})$", mac)) :
+			raise ValueError(f"the \"mac\" argument with value \"{mac}\" is invalid")
+		return Mac(*[int(i, base = 16) for i in mac.split(":")])
+
+	def __get_ip(self, ip : str) -> IPv4 :
+		if (not re.match(r"^(?:(?:25[0-5]|2[0-4]\d|1?\d{1,2})(?:\.(?!$)|$)){4}$", mac)) :
+			raise ValueError(f"the \"ip\" argument with value \"{ip}\" is invalid")
+		return IPv4(*[int(i) for i in ip.split(".")])
+
+	def __del__(self) :
+		os.close(self.context.sockfd.value)
 
 class NodeProbeCore :
-	def __init__(self) :
-		pass
+	def __init__(self, ip : str) :
+		self.ip = ip
 
 
-class NodeProbeIface(NodeProbeCore) :
+class NodeProbeIface :
 	def __init__(self) :
-		super()
 
 	@staticmethod
 	def check_input(value : str) -> bool :
-		return "i" in value
+		return True
 
--- nodeprobe_types.py
+++ nodeprobe_types.py
@@ -1,4 +1,5 @@
 from ctypes import *
+from subprocess import getoutput
 
 TITLE = "NodeProbe-TUI"
 
@@ -11,15 +12,18 @@ Manual
 
 TABLE_KEYS = ("IP", "MAC", "RTT")
 
+Mac = c_ubyte * 6
+IPv4 = c_ubyte * 4
+
 class arpman_context_t(Structure) :
 	_fields_ = [
 		("sockfd", c_int),
 		("ifindex", c_int),
 		("timeout", c_int),
-		("src_mac", c_ubyte * 6),
-		("out_mac", c_ubyte * 6),
-		("src_ip", c_ubyte * 4),
-		("dst_ip", c_ubyte * 4)
+		("src_mac", Mac),
+		("out_mac", Mac),
+		("src_ip", IPv4),
+		("dst_ip", IPv4)
 	]
 
 class icmpman_context_t(Structure) :
@@ -29,10 +33,10 @@ class icmpman_context_t(Structure) :
 		("timeout", c_int),
 		("id", c_ushort),
 		("mtu_size", c_size_t),
-		("src_mac", c_ubyte * 6),
-		("dst_mac", c_ubyte * 6),
-		("src_ip", c_ubyte * 4),
-		("dst_ip", c_ubyte * 4)
+		("src_mac", Mac),
+		("dst_mac", Mac),
+		("src_ip", IPv4),
+		("dst_ip", IPv4)
 	]
 
 class tcpman_context_t(Structure) :
@@ -41,10 +45,10 @@ class tcpman_context_t(Structure) :
 		("ifindex", c_int),
 		("timeout", c_int),
 		("mtu_size", c_size_t),
-		("src_mac", c_ubyte * 6),
-		("dst_mac", c_ubyte * 6),
-		("src_ip", c_ubyte * 4),
-		("dst_ip", c_ubyte * 4),
+		("src_mac", Mac),
+		("dst_mac", Mac),
+		("src_ip", IPv4),
+		("dst_ip", IPv4),
 		("src_port", c_ushort),
 		("dst_port", c_ushort)
 	]
@@ -55,10 +59,13 @@ class udpman_context_t(Structure) :
 		("ifindex", c_int),
 		("timeout", c_int),
 		("mtu_size", c_size_t),
-		("src_mac", c_ubyte * 6),
-		("dst_mac", c_ubyte * 6),
-		("src_ip", c_ubyte * 4),
-		("dst_ip", c_ubyte * 4),
+		("src_mac", Mac),
+		("dst_mac", Mac),
+		("src_ip", IPv4),
+		("dst_ip", IPv4),
 		("src_port", c_ushort),
 		("dst_port", c_ushort)
 	]
+
+
+nodeprobe = cdll.LoadLibrary(f"library/libnodeprobe-{getoutput("ldd --version 2>&1 | grep -qi musl && echo musl || echo glibc")}.so")
